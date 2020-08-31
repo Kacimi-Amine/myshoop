2045e01b9b06529407342e75ef4aac816edf4f70
kacimi amine <kacimi.aminee@gmail.com>
2020-08-31 14:36:00 +0100

    test_form_vs

--- resources/views/Admin/categories/listerCategorie.blade.php
+++ resources/views/Admin/categories/listerCategorie.blade.php
@@ -55,7 +55,7 @@
                                 </button>
                                 </div>
                                 <div class="modal-body">
-                                    Voulez-vous supprimer Categorie: {{$a->nom}} , ainsi ses sous_categories 
+                                    Voulez-vous supprimer Categorie: {{$a->nom}} , ainsi ses sous_categories  ?
                                 </div>
                                 <div class="modal-footer">
                                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
